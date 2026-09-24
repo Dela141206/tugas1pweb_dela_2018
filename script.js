e3ce4e39025ab5427e3bd990c2016f7fbe33df6c
Dela <252410102018@mail.unej.ac.id>
2026-09-24 10:43:54 +0700

    Upload tugas pweb 1 dela

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("aboutmeBtn").addEventListener("click", function() {
+  alert("Ayo berkenalan denganku! Tekan \"Tentang Saya\" untuk tau lebih banyak tentangku.");
+});
+
+document.getElementById("socialBtn").addEventListener("click", function() {
+  alert("Mari mutualan di sosial media! Tekan \"Sosial Media\" untuk mengetahui sosial mediaku.");
+});
